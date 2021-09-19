86a77276bfbafeffee6642626b662a39f9a48714
Samyak-Bumb <vijaybumb0575@gmail.com>
2021-09-19 12:10:02 +0530

    Create Dinosaur-Script.js

---
+++ Dinosaur-Script.js
@@ -0,0 +1,12 @@
+
+// *Now* Not Created By Samyak (Only Written by Samyak😂😂)
+
+  var original=Runner.prototype.gameOver=function(){}
+
+/* 
+Steps to Run This Code :-
+1. In Chrome Browser type --> chrome://dino
+2. Press Ctrl+Shift+I (Inspect) or Right Click and Click on Inspect
+3. Go in Console
+4. Paste The Code & Hit Enter 
+*/
